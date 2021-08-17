059c928d5c5c912a1e6bda141816806d11ada386
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2021-08-18 07:45:01 +0900

    destroy test delete

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,11 +38,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to user_url(@user)
   end
 
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete user_url(@user)
-    end
+  # test "should destroy user" do
+  #   assert_difference('User.count', -1) do
+  #     delete user_url(@user)
+  #   end
 
-    assert_redirected_to users_url
-  end
+  #   assert_redirected_to users_url
+  # end
 end
