b95d34f257af78b1032e81c72c2206ffa7c7f110
toyo-takechan <toyo-takechan@mtf.biglobe.ne.jp>
2021-08-18 07:22:46 +0900

    home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :messages
   resources :users
   # root to: 'application#hello'
-  root to: 'messages#index'
+  # root to: 'messages#index'
+  root to: 'home#index'
 end
   
